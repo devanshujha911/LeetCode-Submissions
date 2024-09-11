9b78e124b6cb36a712789a190eecb427da01c1ba
Devanshu Jha <83602156+devanshujha911@users.noreply.github.com>
2024-09-12 03:17:27 +0530

    Time: 4 ms (72.95%) | Memory: 44.7 MB (29.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,23 +3,34 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            if(map1.get(s.charAt(i))==null){
-                map1.put(s.charAt(i), 1);
-            }else{
-                map1.put(s.charAt(i), map1.get(s.charAt(i))+1);
-            }
+
+    //     //USING HASHMAP
+    //     HashMap<Character, Integer> map1 = new HashMap<>();
+    //     for(int i=0; i<s.length(); i++){
+    //         if(map1.get(s.charAt(i))==null){
+    //             map1.put(s.charAt(i), 1);
+    //         }else{
+    //             map1.put(s.charAt(i), map1.get(s.charAt(i))+1);
+    //         }
             
-        }
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for(int i=0; i<t.length(); i++){
-            if(map2.get(t.charAt(i))==null){
-                map2.put(t.charAt(i), 1);
-            }else{
-                map2.put(t.charAt(i), map2.get(t.charAt(i))+1);
-            }
-        }
-        return map1.equals(map2);
+    //     }
+    //     HashMap<Character, Integer> map2 = new HashMap<>();
+    //     for(int i=0; i<t.length(); i++){
+    //         if(map2.get(t.charAt(i))==null){
+    //             map2.put(t.charAt(i), 1);
+    //         }else{
+    //             map2.put(t.charAt(i), map2.get(t.charAt(i))+1);
+    //         }
+    //     }
+    //     return map1.equals(map2);
+    
+    //USING CharArray
+
+        char[] chs = s.toCharArray();
+        char[] cht = t.toCharArray();
+        Arrays.sort(chs);
+        Arrays.sort(cht);
+        return Arrays.equals(chs, cht);
+
     }
 }
