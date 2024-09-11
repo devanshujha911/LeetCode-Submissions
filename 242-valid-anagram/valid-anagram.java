c18cfc91794f1c8cc04d006e598ea576e680546e
Devanshu Jha <83602156+devanshujha911@users.noreply.github.com>
2024-09-12 03:12:58 +0530

    Time: 21 ms (10.66%) | Memory: 44.6 MB (39.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         HashMap<Character, Integer> map1 = new HashMap<>();
         for(int i=0; i<s.length(); i++){
             if(map1.get(s.charAt(i))==null){
